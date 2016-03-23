2003039a33f8e71789bf87b5cb8d9d6a73d533c4
ldp <da_peng@100yibuy.com>
2016-03-23 08:50:11 +0800

    add square vc

---
+++ petegg/Classes/ViewControlls/MainTab/Square/RecommendViewController.m
@@ -0,0 +1,39 @@
+//
+//  RecommendViewController.m
+//  petegg
+//
+//  Created by ldp on 16/3/23.
+//  Copyright © 2016年 sego. All rights reserved.
+//
+
+#import "RecommendViewController.h"
+
+@interface RecommendViewController ()
+
+@end
+
+@implementation RecommendViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+    
+    self.view.backgroundColor = [UIColor blackColor];
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
