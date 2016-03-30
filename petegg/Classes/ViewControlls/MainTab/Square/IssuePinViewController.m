f0a6667451bfe03a89ada9dfd219dff1646fa4b1
ChenZeXuan <610121499@qq.com>
2016-03-30 18:27:43 +0800

    添加删除图片搞定了

--- petegg/Classes/ViewControlls/MainTab/Square/IssuePinViewController.m
+++ petegg/Classes/ViewControlls/MainTab/Square/IssuePinViewController.m
@@ -8,12 +8,16 @@
 
 #import "IssuePinViewController.h"
 
-@interface IssuePinViewController ()
+@interface IssuePinViewController ()<UIImagePickerControllerDelegate,UINavigationControllerDelegate>
 @property (nonatomic,strong)UITextView * topTextView;
 @property (nonatomic,strong)UIView * downWithView;
 @property (nonatomic,strong)UIButton * coverButton;
 @property (nonatomic,strong)UIView * littleDownView;
 @property (nonatomic,strong)NSMutableArray * imageArray;
+@property(nonatomic,strong)UIImagePickerController * imagePicker;
+@property (nonatomic,strong)UIButton * imageButtones;
+
+@property(nonatomic,strong)UIView * bigView;
 
 @end
 
@@ -22,6 +26,8 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     self.view.backgroundColor = [UIColor grayColor];
+    _imagePicker =[[UIImagePickerController alloc]init];
+    _imagePicker.delegate= self;
     [self setTitle:@"发布"];
    
 }
@@ -32,6 +38,7 @@
     [self.view addSubview:_topTextView];
     _imageArray = [[NSMutableArray alloc]init];
     [_imageArray addObject:_firstImage];
+  
     [self addImageS];
     
     
@@ -43,34 +50,73 @@
 }
 
 -(void)addImageS{
-    //    UIButton * testButton = [[UIButton alloc]initWithFrame:CGRectMake(100, 100, 100, 100)];
-    //    //testButton.backgroundColor = [UIColor redColor];
-    //    [testButton setImage:_firstImage forState:UIControlStateNormal];
-    //    [self.view addSubview:testButton];
-    //    [testButton addTarget:self action:@selector(openDownView) forControlEvents:UIControlEventTouchUpInside];
-//    NSMutableArray * array = [[NSMutableArray alloc]init];
-//    array = [NSMutableArray arrayWithArray:_imageArray];
     
+    _bigView = [[UIView alloc]initWithFrame:CGRectMake(0, 220, 375, 200)];
+    _bigView.backgroundColor = self.view.backgroundColor;
+    [self.view addSubview:_bigView];
     [_imageArray addObject:[UIImage imageNamed:@"addImage.png"]];
         for (int i = 0 ; i < _imageArray.count; i++) {
-            UIButton * buttones = [[UIButton alloc]initWithFrame:CGRectMake(0 + i * 90, 220, 80, 80)];
-            [buttones setImage:_imageArray[i] forState:UIControlStateNormal];
-            [self.view addSubview:buttones];
-            buttones.tag = i;
-            [buttones addTarget:self action:@selector(buttonTouch:) forControlEvents:UIControlEventTouchUpInside];
+            _imageButtones = [[UIButton alloc]initWithFrame:CGRectMake(12.5 + i * 90, 0, 80, 80)];
+            [_imageButtones setImage:_imageArray[i] forState:UIControlStateNormal];
+            [_bigView addSubview:_imageButtones];
+            _imageButtones.tag = i;
+            [_imageButtones addTarget:self action:@selector(buttonTouch:) forControlEvents:UIControlEventTouchUpInside];
         }
 }
 
 -(void)buttonTouch:(UIButton *)sender{
-    if (sender.tag == _imageArray.count - 1) {
-        [self openDownView];
+    
+    
+    NSInteger i = _imageArray.count;
+    if (sender.tag == i - 1 ) {
+        if (_imageArray.count > 4) {
+            NSLog(@"不能大于4张");
+            return;
+        }
+        [self openDownImageView];
     }else{
-        NSLog(@"进入大图模式");
+        UIAlertController *  alert = [UIAlertController alertControllerWithTitle:nil message:nil preferredStyle:UIAlertControllerStyleActionSheet];
+        
+          [alert addAction:[UIAlertAction actionWithTitle:@"预览" style:UIAlertActionStyleDefault handler:^(UIAlertAction * _Nonnull action) {
+              
+              NSLog(@"预览");
+          }]];
+        
+        [alert addAction:[UIAlertAction actionWithTitle:@"删除" style:UIAlertActionStyleDefault handler:^(UIAlertAction * _Nonnull action) {
+            [sender removeFromSuperview];
+            [_imageArray removeObjectAtIndex:sender.tag];
+            [_bigView removeFromSuperview];
+            [self paixuImageButton];
+           
+        }]];
+
+            [alert addAction:[UIAlertAction actionWithTitle:@"取消" style:UIAlertActionStyleCancel handler:^(UIAlertAction * _Nonnull action) {
+                
+            }]];
+        [self presentViewController:alert animated:YES completion:nil];
+    }
+        
+}
+//删除照片后重新排序
+-(void)paixuImageButton{
+
+  
+    _bigView = [[UIView alloc]initWithFrame:CGRectMake(0, 220, 375, 200)];
+    _bigView.backgroundColor = self.view.backgroundColor;
+    [self.view addSubview:_bigView];
+    for (int i = 0 ; i < _imageArray.count; i++) {
+        _imageButtones = [[UIButton alloc]initWithFrame:CGRectMake(12.5 + i * 90, 220, 80, 80)];
+        [_imageButtones setImage:_imageArray[i] forState:UIControlStateNormal];
+        [self.view addSubview:_imageButtones];
+        _imageButtones.tag = i;
+        [_imageButtones addTarget:self action:@selector(buttonTouch:) forControlEvents:UIControlEventTouchUpInside];
     }
 
 }
 
--(void)openDownView{
+
+
+-(void)openDownImageView{
     _downWithView = [[UIView alloc]initWithFrame:CGRectMake(0, 667, 375, 80)];
     _littleDownView = [[UIView alloc]initWithFrame:CGRectMake(0, 667, 375, 40)];
     _coverButton = [[UIButton alloc]initWithFrame:CGRectMake(0, 0, 375, 667)];
@@ -86,16 +132,19 @@
         [[UIApplication sharedApplication].keyWindow addSubview:_littleDownView];
         [[UIApplication sharedApplication].keyWindow addSubview:_downWithView];
     }];
-    NSArray * nameArray = @[NSLocalizedString(@"photograph", nil),NSLocalizedString(@"photoalbum", nil),NSLocalizedString(@"repository", nil),NSLocalizedString(@"lvideo", nil)];
+    NSArray * nameArray = @[NSLocalizedString(@"photograph", nil),NSLocalizedString(@"photoalbum", nil)];
     for (int i = 0; i < 2; i++) {
         UILabel * lineLabel = [[UILabel alloc]initWithFrame:CGRectMake(0, 0 + i * 40, 375, 1)];
         lineLabel.backgroundColor = GRAY_COLOR;
         [_downWithView addSubview:lineLabel];
-
+        
         UIButton * downButtones = [[UIButton alloc]initWithFrame:CGRectMake(0, 0 + i * 40, 375, 40)];
-        
-        
-        
+        [downButtones setTitle:nameArray[i] forState:UIControlStateNormal];
+        [downButtones setTitleColor:[UIColor blackColor] forState:UIControlStateNormal];
+        downButtones.titleLabel.font = [UIFont systemFontOfSize:14];
+        [_downWithView addSubview:downButtones];
+        downButtones.tag = i;
+        [downButtones addTarget:self action:@selector(imageButtonTouch:) forControlEvents:UIControlEventTouchUpInside];
         
     }
     
@@ -104,13 +153,73 @@
 
 
 -(void)hideButton:(UIButton *)sender{
-    sender.hidden = YES;
+    _coverButton.hidden = YES;
     [UIView animateWithDuration:0.3 animations:^{
         _downWithView.frame = CGRectMake(0, 667, 375, 80);
         _littleDownView.frame = CGRectMake(0, 667, 375, 40);
     }];
     
+}
 
+-(void)imageButtonTouch:(UIButton *)sender{
+    if (sender.tag == 0) {
+        [self takePhoto];
+    }else{
+        [self loacalPhoto];
+    }
+
+}
+
+
+#pragma mark - Uiimagepicker
+
+// 拍照
+- (void)takePhoto
+{
+    [self hideButton:nil];
+    NSArray * mediaty = [UIImagePickerController availableMediaTypesForSourceType:UIImagePickerControllerSourceTypeCamera];
+    if ([UIImagePickerController isSourceTypeAvailable:UIImagePickerControllerSourceTypeCamera]) {
+        _imagePicker.sourceType = UIImagePickerControllerSourceTypeCamera;
+        _imagePicker.mediaTypes = @[mediaty[0]];
+        //设置相机模式：1摄像2录像
+        _imagePicker.cameraCaptureMode = UIImagePickerControllerCameraCaptureModePhoto;
+        //使用前置还是后置摄像头
+        _imagePicker.cameraDevice = UIImagePickerControllerCameraDeviceRear;
+        //闪光模式
+        _imagePicker.cameraFlashMode = UIImagePickerControllerCameraFlashModeAuto;
+        _imagePicker.allowsEditing = YES;
+    }else
+    {
+        NSLog(@"打开摄像头失败");
+    }
+    [self presentViewController:_imagePicker animated:YES completion:nil];
+    //[self.navigationController pushViewController:_imagePicker animated:YES];
+    
+}
+//相册选取
+
+- (void)loacalPhoto
+{
+    [self hideButton:nil];
+    NSArray * mediaTypers = [UIImagePickerController availableMediaTypesForSourceType:UIImagePickerControllerSourceTypePhotoLibrary];
+    if ([UIImagePickerController isSourceTypeAvailable:UIImagePickerControllerSourceTypePhotoLibrary]) {
+        _imagePicker.sourceType = UIImagePickerControllerSourceTypePhotoLibrary;
+        _imagePicker.mediaTypes = @[mediaTypers[0],mediaTypers[1]];
+        _imagePicker.allowsEditing = YES;
+    }
+    [self presentViewController:_imagePicker animated:NO completion:nil];
+    
+}
+
+//得到图片之后的处理
+- (void)imagePickerController:(UIImagePickerController *)picker didFinishPickingMediaWithInfo:(NSDictionary *)info
+{
+    [_imageArray removeLastObject];
+    UIImage * getImage = [info objectForKey:UIImagePickerControllerOriginalImage];
+    [_imageArray addObject:getImage];
+    [self dismissViewControllerAnimated:NO completion:nil];
+    [self addImageS];
+    
 }
 
 
