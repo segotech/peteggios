5b4d3d8b99eef4dacab1b45a2ec70a117039f030
yuleicoco <1040341999@qq.com>
2016-04-02 18:32:43 +0800

    图像处理

--- petegg/Classes/ViewControlls/MainTab/Personal/PersonalViewController.m
+++ petegg/Classes/ViewControlls/MainTab/Personal/PersonalViewController.m
@@ -8,6 +8,7 @@
 
 #import "PersonalViewController.h"
 #import "personTableViewCell.h"
+#import <Accelerate/Accelerate.h>
 
 @interface PersonalViewController()
 
@@ -55,15 +56,17 @@
     UIView  * _headView = [[UIView alloc]initWithFrame:CGRectMake(0, 0, 375, 200)];
     _headView.backgroundColor = [UIColor whiteColor];
     UIImageView *bgImgView = [[UIImageView alloc] initWithFrame:CGRectMake(0,0,375,200)];
-    //applyBlurRadius
-    UIImage * image =[[UIImage alloc]init];
+    bgImgView.image = [self blurryImage:[self cutImage:[UIImage imageNamed:@"ceishi.jpg"]] withBlurLevel:0.2];
+    [_headView addSubview:bgImgView];
+    [_headView sendSubviewToBack:bgImgView];
+    
  
     
     [self.view addSubview:_headView];
     
     // 头像
     _heandBtn =[[UIButton alloc]initWithFrame:CGRectMake(_headView.center.x-40, self.view.origin.y+30, 80, 80)];
-    _heandBtn.backgroundColor =[UIColor redColor];
+    [_heandBtn setImage:[UIImage imageNamed:@"ceishi.jpg"] forState:UIControlStateNormal];
     [_heandBtn.layer setMasksToBounds:YES];
     [_heandBtn.layer setCornerRadius:40]; //设置矩形四个圆角半径
     _heandBtn.userInteractionEnabled = YES;
@@ -71,29 +74,28 @@
     
     [_headView addSubview:_heandBtn];
     
-//    /**
-//        点赞  名字
-//     */
-//    _nameLabel = [[UILabel alloc]initWithFrame:CGRectMake(0, 0, 200, 20)];
-//    _nameLabel.text = @"我是余磊 我爱打麻将";
-//    _nameLabel.center = CGPointMake(self.view.center.x,_heandBtn.frame.origin.y+100);
-//    _nameLabel.font = [UIFont systemFontOfSize:14];
-//    _nameLabel.textAlignment = NSTextAlignmentCenter;
-//    _nameLabel.textColor = [UIColor blackColor];
-//    [self.view addSubview:_nameLabel];
-//    
-//    UIImageView * message1 =[[UIImageView alloc]initWithFrame:CGRectMake(240, -20, 15,15)];
-//    message1.image =[UIImage imageNamed:@"while_good"];
-//    [self.view addSubview:message1];
-//    
-//    UILabel * goodLabel = [[UILabel alloc]initWithFrame:CGRectMake(260, -23, 100, 20)];
-//    goodLabel.textColor = [UIColor grayColor];
-//    goodLabel.font = [UIFont systemFontOfSize:12];
-//    [self.view addSubview:goodLabel];
+    /**
+        点赞  名字
+     */
+    _nameLabel = [[UILabel alloc]initWithFrame:CGRectMake(0, 0, 200, 20)];
+    _nameLabel.text = @"我是余磊 我爱打麻将";
+    _nameLabel.center = CGPointMake(self.view.center.x,_heandBtn.frame.origin.y+160);
+    _nameLabel.font = [UIFont systemFontOfSize:15];
+    _nameLabel.textAlignment = NSTextAlignmentCenter;
+    _nameLabel.textColor = [UIColor whiteColor];
+    [self.view addSubview:_nameLabel];
+
     
+ 
   
 
-
+   /**
+    *  循环控件 3个12
+    *
+    */
+    
+    
+    
     
     
    
@@ -383,10 +385,137 @@
     CGImageRelease(cgImage);
     return returnImage;
 }
+
+
 /**
- *  @2 第二种方法
  *
+ *  @2 第二种方法
+ *  @param image
+ *  @param blur  模糊度
+ *
+ *  @return image
+ */
+- (UIImage *)blurryImage:(UIImage *)image withBlurLevel:(CGFloat)blur {
+    //模糊度,
+    if ((blur < 0.1f) || (blur > 2.0f)) {
+        blur = 0.5f;
+    }
+    
+    //boxSize必须大于0
+    int boxSize = (int)(blur * 100);
+    boxSize -= (boxSize % 2) + 1;
+    NSLog(@"boxSize:%i",boxSize);
+    //图像处理
+    CGImageRef img = image.CGImage;
+    //需要引入#import <Accelerate/Accelerate.h>
+    /*
+     本文档介绍了Accelerate Framework，其中包含C语言应用程序接口（API）的向量和矩阵数学，数字信号处理，大量处理和图像处理。
+     */
+    
+    //图像缓存,输入缓存，输出缓存
+    vImage_Buffer inBuffer, outBuffer;
+    vImage_Error error;
+    //像素缓存
+    void *pixelBuffer;
+    
+    //数据源提供者，Defines an opaque type that supplies Quartz with data.
+    CGDataProviderRef inProvider = CGImageGetDataProvider(img);
+    // provider’s data.
+    CFDataRef inBitmapData = CGDataProviderCopyData(inProvider);
+    
+    //宽，高，字节/行，data
+    inBuffer.width = CGImageGetWidth(img);
+    inBuffer.height = CGImageGetHeight(img);
+    inBuffer.rowBytes = CGImageGetBytesPerRow(img);
+    inBuffer.data = (void*)CFDataGetBytePtr(inBitmapData);
+    
+    //像数缓存，字节行*图片高
+    pixelBuffer = malloc(CGImageGetBytesPerRow(img) * CGImageGetHeight(img));
+    
+    outBuffer.data = pixelBuffer;
+    outBuffer.width = CGImageGetWidth(img);
+    outBuffer.height = CGImageGetHeight(img);
+    outBuffer.rowBytes = CGImageGetBytesPerRow(img);
+    
+    
+    // 第三个中间的缓存区,抗锯齿的效果
+    void *pixelBuffer2 = malloc(CGImageGetBytesPerRow(img) * CGImageGetHeight(img));
+    vImage_Buffer outBuffer2;
+    outBuffer2.data = pixelBuffer2;
+    outBuffer2.width = CGImageGetWidth(img);
+    outBuffer2.height = CGImageGetHeight(img);
+    outBuffer2.rowBytes = CGImageGetBytesPerRow(img);
+    
+    //Convolves a region of interest within an ARGB8888 source image by an implicit M x N kernel that has the effect of a box filter.
+    error = vImageBoxConvolve_ARGB8888(&inBuffer, &outBuffer2, NULL, 0, 0, boxSize, boxSize, NULL, kvImageEdgeExtend);
+    error = vImageBoxConvolve_ARGB8888(&outBuffer2, &inBuffer, NULL, 0, 0, boxSize, boxSize, NULL, kvImageEdgeExtend);
+    error = vImageBoxConvolve_ARGB8888(&inBuffer, &outBuffer, NULL, 0, 0, boxSize, boxSize, NULL, kvImageEdgeExtend);
+    
+    
+    if (error) {
+        NSLog(@"error from convolution %ld", error);
+    }
+    
+    //    NSLog(@"字节组成部分：%zu",CGImageGetBitsPerComponent(img));
+    //颜色空间DeviceRGB
+    CGColorSpaceRef colorSpace = CGColorSpaceCreateDeviceRGB();
+    //用图片创建上下文,CGImageGetBitsPerComponent(img),7,8
+    CGContextRef ctx = CGBitmapContextCreate(
+                                             outBuffer.data,
+                                             outBuffer.width,
+                                             outBuffer.height,
+                                             8,
+                                             outBuffer.rowBytes,
+                                             colorSpace,
+                                             CGImageGetBitmapInfo(image.CGImage));
+    
+    //根据上下文，处理过的图片，重新组件
+    CGImageRef imageRef = CGBitmapContextCreateImage (ctx);
+    UIImage *returnImage = [UIImage imageWithCGImage:imageRef];
+    
+    //clean up
+    CGContextRelease(ctx);
+    CGColorSpaceRelease(colorSpace);
+    
+    free(pixelBuffer);
+    free(pixelBuffer2);
+    CFRelease(inBitmapData);
+    
+    //    CGColorSpaceRelease(colorSpace);
+    CGImageRelease(imageRef);
+    
+    return returnImage;
+}
+
+/**
+ *  处理图片
  */
 
+- (UIImage *)cutImage:(UIImage*)image
+{
+    
+    CGSize newSize;
+    CGImageRef imageRef = nil;
+    UIImageView *_headerView =[[UIImageView alloc]initWithFrame:CGRectMake(0, 0, 375, 200)];
+    
+    
+    if ((image.size.width / image.size.height) < (_headerView.bounds.size.width / _headerView.bounds.size.height)) {
+        newSize.width = image.size.width;
+        newSize.height = image.size.width * _headerView.bounds.size.height / _headerView.bounds.size.width;
+        
+        imageRef = CGImageCreateWithImageInRect([image CGImage], CGRectMake(0, fabs(image.size.height - newSize.height) / 2, newSize.width, newSize.height));
+        
+    } else {
+        newSize.height = image.size.height;
+        newSize.width = image.size.height * _headerView.bounds.size.width / _headerView.bounds.size.height;
+        
+        imageRef = CGImageCreateWithImageInRect([image CGImage], CGRectMake(fabs(image.size.width - newSize.width) / 2, 0, newSize.width, newSize.height));
+        
+    }
+    UIImage * newnewimage = [UIImage imageWithCGImage:imageRef];
+    CGImageRelease(imageRef);
+    return newnewimage;
+    //return [UIImage imageWithCGImage:imageRef];
+}
 
 @end
