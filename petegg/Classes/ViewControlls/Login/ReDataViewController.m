9c4b9f909985d7c19de2330794b67e4ac29f8ab7
ChenZeXuan <610121499@qq.com>
2016-04-05 18:29:44 +0800

    创建了注册和资料

---
+++ petegg/Classes/ViewControlls/Login/ReDataViewController.m
@@ -0,0 +1,24 @@
+//
+//  ReDataViewController.m
+//  petegg
+//
+//  Created by czx on 16/4/5.
+//  Copyright © 2016年 sego. All rights reserved.
+//
+
+#import "ReDataViewController.h"
+
+@interface ReDataViewController ()
+
+@end
+
+@implementation ReDataViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+
+
+@end
---
+++ petegg/Classes/ViewControlls/Login/RegiestViewController.m
@@ -0,0 +1,24 @@
+//
+//  RegiestViewController.m
+//  petegg
+//
+//  Created by czx on 16/4/5.
+//  Copyright © 2016年 sego. All rights reserved.
+//
+
+#import "RegiestViewController.h"
+
+@interface RegiestViewController ()
+
+@end
+
+@implementation RegiestViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+
+
+@end
