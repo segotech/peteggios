41a78eca3cb8dfa0909e83e63bf42076a8b771ab
yuleicoco <1040341999@qq.com>
2016-04-18 17:26:33 +0800

    no message

--- petegg/Classes/ViewControlls/Login/RegiestViewController.h
+++ petegg/Classes/ViewControlls/Login/RegiestViewController.h
@@ -7,7 +7,8 @@
 //
 
 #import <UIKit/UIKit.h>
+#import "BaseViewController.h"
 
-@interface RegiestViewController : UIViewController
+@interface RegiestViewController : BaseViewController
 
 @end
--- petegg/Classes/ViewControlls/Login/RegiestViewController.m
+++ petegg/Classes/ViewControlls/Login/RegiestViewController.m
@@ -19,12 +19,16 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    self.title = @"注册";
     self.view.backgroundColor= [UIColor whiteColor];
-    [self initUserface];
+     [self setNavTitle: NSLocalizedString(@"regist", nil)];
+    
 }
 
--(void)initUserface{
+
+- (void)setupView{
+    
+    [super setupView];
+    
     NSArray * namearray = @[@"账号:",@"验证码:",@"密码:",@"确认密码:",];
     NSArray * placeArray = @[@"请输入账号",@"请输入验证码",@"请输入密码",@"请确认密码"];
     for (int i = 0 ; i < 4; i++ ) {
@@ -40,22 +44,82 @@
         [self.view addSubview:lineLabeles];
         
         _textFieldes = [[UITextField alloc]initWithFrame:CGRectMake(120 * W_Wide_Zoom,  68 + i * 50 * W_Hight_Zoom, 200 * W_Wide_Zoom, 40 * W_Hight_Zoom)];
-        //_textFieldes.backgroundColor = [UIColor blackColor];
         _textFieldes.placeholder = placeArray[i];
         [_textFieldes setValue:[UIFont systemFontOfSize:14] forKeyPath:@"_placeholderLabel.font"];
         [_textFieldes setValue:[UIColor lightGrayColor] forKeyPath:@"_placeholderLabel.textColor"];
         _textFieldes.tag = i + 33;
+        
+        if (i>=3) {
+            UIButton * showBtn =[[UIButton alloc]initWithFrame:CGRectMake(320, 218+(i-3+50), 40, 40)];
+            [showBtn setImage:[UIImage imageNamed:@"showPs.png"] forState:UIControlStateNormal];
+            [self.view  addSubview:showBtn];
+            
+        }
+        
         [self.view addSubview:_textFieldes];
         
     }
-
+    
     _securityButton = [[UIButton alloc]initWithFrame:CGRectMake(270 * W_Wide_Zoom, 118 * W_Hight_Zoom, 100 * W_Wide_Zoom, 30 * W_Hight_Zoom)];
     _securityButton.backgroundColor = GREEN_COLOR;
     [_securityButton setTitle:@"获取验证码" forState:UIControlStateNormal];
     _securityButton.titleLabel.font = [UIFont systemFontOfSize:14];
     _securityButton.layer.cornerRadius = 5;
+    [_securityButton addTarget:self action:@selector(passwordCode:) forControlEvents:UIControlEventTouchUpInside];
     [self.view addSubview:_securityButton];
-
+    
+    
+    
+    //注册
+    
+    UIButton * registBtn =[[UIButton alloc]initWithFrame:CGRectMake(0, 0, 250, 40)];
+    registBtn.backgroundColor =GREEN_COLOR;
+    registBtn.layer.cornerRadius =  6;
+    registBtn.center = self.view.center;
+    [registBtn setTitle:@"注册" forState:UIControlStateNormal];
+    registBtn.titleLabel.font =[UIFont systemFontOfSize:16];
+    [registBtn addTarget:self action:@selector(registBtn:) forControlEvents:UIControlEventTouchUpInside];
+    [self.view addSubview:registBtn];
+    
+    
+    
+    
 }
 
+
+- (void)setupData
+{
+    [super setupData];
+    
+    
+}
+
+
+
+/**
+ *   验证码
+ */
+
+
+- (void)passwordCode:(UIButton *)sender
+{
+    
+    
+    
+    
+}
+
+
+
+/**
+ *  注册
+ */
+
+
+- (void)registBtn:(UIButton *)sender
+{
+    
+    
+    
+}
 @end
