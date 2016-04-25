38a2357ab526f7e06c427f19d13e3029423d60d7
yuleicoco <1040341999@qq.com>
2016-04-25 17:20:54 +0800

    sephone

---
+++ petegg/Classes/AppDelegate/AppDelegate+Sephone.m
@@ -0,0 +1,22 @@
+//
+//  AppDelegate+Sephone.m
+//  petegg
+//
+//  Created by yulei on 16/4/25.
+//  Copyright © 2016年 sego. All rights reserved.
+//
+
+#import "AppDelegate+Sephone.h"
+
+@implementation AppDelegate (Sephone)
+
+- (void)initSephoneVoip:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
+{
+
+    //Sephone
+    
+    
+    
+    
+}
+@end
