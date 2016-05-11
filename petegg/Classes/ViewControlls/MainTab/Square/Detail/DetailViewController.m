d94ed813f265ca97ed1ec886936e07e93383945d
yuleicoco <1040341999@qq.com>
2016-05-11 10:32:02 +0800

    no message

--- petegg/Classes/ViewControlls/MainTab/Square/Detail/DetailViewController.m
+++ petegg/Classes/ViewControlls/MainTab/Square/Detail/DetailViewController.m
@@ -167,6 +167,9 @@ NSString * const kDetailVideoCellID = @"DetailVideoCell";
             
         }];
         
+//        [shareBtn addTarget:self action:@selector(shareBox) forControlEvents:
+//         UIControlEventTouchUpInside];
+        
         //评论
         UIButton* commentBtn = [UIButton buttonWithType:UIButtonTypeCustom];
         [commentBtn setImage:[UIImage imageNamed:@"commentBtn"] forState:UIControlStateNormal];
@@ -259,16 +262,16 @@ NSString * const kDetailVideoCellID = @"DetailVideoCell";
     //（注意：图片必须要在Xcode左边目录里面，名称必须要传正确，如果要分享网络图片，可以这样传iamge参数
     
 
-    NSString * strUrl =[NSString stringWithFormat:@"http://180.97.81.213:15102/clientAction.do?method=client&nextPage=/s/sproutpet/article.jsp&stid=%@&mid=%@&access=outside",self.stid,[AccountManager sharedAccountManager].loginModel.mid];
+   // NSString * strUrl =[NSString stringWithFormat:@"http://180.97.81.213:15102/clientAction.do?method=client&nextPage=/s/sproutpet/article.jsp&stid=%@&mid=%@&access=outside",self.stid,[AccountManager sharedAccountManager].loginModel.mid];
     
     if (imageArray) {
         
         NSMutableDictionary *shareParams = [NSMutableDictionary dictionary];
         [shareParams
-        SSDKSetupShareParamsByText:@"赛果不倒蛋,远程遥控逗宠神器"
-         images:imageArray
-         url:[NSURL URLWithString:strUrl]
-         title:@"赛果逗码分享"
+         SSDKSetupShareParamsByText:[NSString stringWithFormat:@"赛果分享http://180.97.81.213:15102/clientAction.do?method=client&nextPage=/s/sproutpet/article.jsp&stid=%@&mid=%@&access=outside",self.stid,[AccountManager sharedAccountManager].loginModel.mid]
+         images:nil
+         url:nil
+         title:@"赛果分享"
          type:SSDKContentTypeAuto];
         
         // 2、分享（可以弹出我们的分享菜单和编辑界面）
