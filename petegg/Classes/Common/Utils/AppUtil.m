a1b0f9ca1728a90861b649dc1299d7e53ddd01e0
ChenZeXuan <610121499@qq.com>
2016-05-18 15:20:39 +0800

    。。

--- petegg/Classes/Common/Utils/AppUtil.m
+++ petegg/Classes/Common/Utils/AppUtil.m
@@ -21,7 +21,7 @@ static NSString* server = @"http://192.168.43.1:7766/rest/operate";
 //static NSString * getServer3 =@"http://180.97.80.227:15102/";
 // 测试
 //static NSString * getServer3 =@"http://180.97.81.213:15102/";
-static NSString * getServer3 =@"http://www.segoapp.cn/";
+static NSString * getServer3 =@"http://180.97.81.213/";
 static NSString * getServerTest = @"http://180.97.80.227:15102/";
 static NSString * getServerTest1 = @"http://192.168.1.103:8080/sego_v3/";
 
