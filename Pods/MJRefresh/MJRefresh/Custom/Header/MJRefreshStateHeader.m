87d6c568f7318771c6e2afc908b0dbdd206dcdc2
ChenZeXuan <610121499@qq.com>
2016-07-13 10:08:07 +0800

    dadad

--- Pods/MJRefresh/MJRefresh/Custom/Header/MJRefreshStateHeader.m
+++ Pods/MJRefresh/MJRefresh/Custom/Header/MJRefreshStateHeader.m
@@ -61,6 +61,47 @@
     return [NSCalendar currentCalendar];
 }
 
+//#pragma mark key的处理
+//- (void)setLastUpdatedTimeKey:(NSString *)lastUpdatedTimeKey
+//{
+//    [super setLastUpdatedTimeKey:lastUpdatedTimeKey];
+//    
+//    // 如果label隐藏了，就不用再处理
+//    if (self.lastUpdatedTimeLabel.hidden) return;
+//    
+//    NSDate *lastUpdatedTime = [[NSUserDefaults standardUserDefaults] objectForKey:lastUpdatedTimeKey];
+//    
+//    // 如果有block
+//    if (self.lastUpdatedTimeText) {
+//        self.lastUpdatedTimeLabel.text = self.lastUpdatedTimeText(lastUpdatedTime);
+//        return;
+//    }
+//    
+//    if (lastUpdatedTime) {
+//        // 1.获得年月日
+//        NSCalendar *calendar = [self currentCalendar];
+//        NSUInteger unitFlags = NSCalendarUnitYear| NSCalendarUnitMonth | NSCalendarUnitDay |NSCalendarUnitHour |NSCalendarUnitMinute;
+//        NSDateComponents *cmp1 = [calendar components:unitFlags fromDate:lastUpdatedTime];
+//        NSDateComponents *cmp2 = [calendar components:unitFlags fromDate:[NSDate date]];
+//        
+//        // 2.格式化日期
+//        NSDateFormatter *formatter = [[NSDateFormatter alloc] init];
+//        if ([cmp1 day] == [cmp2 day]) { // 今天
+//            formatter.dateFormat = @"Today HH:mm";
+//        } else if ([cmp1 year] == [cmp2 year]) { // 今年
+//            formatter.dateFormat = @"MM-dd HH:mm";
+//        } else {
+//            formatter.dateFormat = @"yyyy-MM-dd HH:mm";
+//        }
+//        NSString *time = [formatter stringFromDate:lastUpdatedTime];
+//        
+//        // 3.显示日期
+//        self.lastUpdatedTimeLabel.text = [NSString stringWithFormat:@"Last update:%@", time];
+//    } else {
+//        self.lastUpdatedTimeLabel.text = @"Last update:：No record";
+//    }
+//}
+
 #pragma mark key的处理
 - (void)setLastUpdatedTimeKey:(NSString *)lastUpdatedTimeKey
 {
@@ -87,7 +128,7 @@
         // 2.格式化日期
         NSDateFormatter *formatter = [[NSDateFormatter alloc] init];
         if ([cmp1 day] == [cmp2 day]) { // 今天
-            formatter.dateFormat = @"Today HH:mm";
+            formatter.dateFormat = @"HH:mm";
         } else if ([cmp1 year] == [cmp2 year]) { // 今年
             formatter.dateFormat = @"MM-dd HH:mm";
         } else {
@@ -101,6 +142,8 @@
         self.lastUpdatedTimeLabel.text = @"Last update:：No record";
     }
 }
+
+
 
 #pragma mark - 覆盖父类的方法
 - (void)prepare
