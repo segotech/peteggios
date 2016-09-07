af76e430eeaec7b51bae849f628a889ea6606e2f
ChenZeXuan <610121499@qq.com>
2016-09-07 16:12:40 +0800

    改了个地址

--- petegg/Classes/Common/Utils/AppUtil.m
+++ petegg/Classes/Common/Utils/AppUtil.m
@@ -25,7 +25,11 @@ static NSString* server = @"http://192.168.1.130:8080/sego_v3";
 //static NSString * getServer3 =@"http://180.97.80.227:15102/";
 // 测试
 //static NSString * getServer3 =@"http://192.168.1.130:8080/sego_v3/";
-static NSString * getServer3 =@"http://180.97.81.213/";
+
+//static NSString * getServer3 =@"http://180.97.81.213/";
+
+static NSString * getServer3 =@"http://180.97.81.213:15101/api/";
+
 static NSString * getServerTest = @"http://180.97.80.227:15102/";
 static NSString * getServerTest1 = @"http://192.168.1.103:8080/sego_v3/";
 
